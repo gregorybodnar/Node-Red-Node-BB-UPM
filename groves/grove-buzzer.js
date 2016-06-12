d5a82385a77fc5ce03e683b956122853fd2b6b69
Baozhu <zuobaozhu@gmail.com>
2016-06-12 09:17:12 +0000

    buzzer works now

--- groves/grove-buzzer.js
+++ groves/grove-buzzer.js
@@ -17,16 +17,15 @@ module.exports = function(RED) {
         this.status({});
 
         var node = this;
-        node.sensor.playSound(node.note);//default note
+        node.sensor.playSound(node.note,1000000);//default note
         this.on('input', function(msg){
             var note = parseInt(msg.payload);
-            note = (note < 2000 || note > 3800) ? 3000 : note; //set to center if note is out of bounds
-            node.sensor.playSound(note);//set to note
+            node.sensor.playSound(note,1000000);//set to note
         });
       
         //clear interval on exit
         this.on("close", function(){
-            node.sensor.playSound(0);//set to note
+            node.sensor.playSound(0,1000000);//set to note
         });
     }
     RED.nodes.registerType('UPM-Grove-Buzzer', groveBuzzer);
